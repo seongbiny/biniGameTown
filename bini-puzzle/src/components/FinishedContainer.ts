24ee6e049641d3d56cfe74adf8d36e21706799db
seongbiny <s30ngb1n@gmail.com>
2025-02-20 18:25:44 +0900

    game 완성

---
+++ bini-puzzle/src/components/FinishedContainer.ts
@@ -0,0 +1,9 @@
+import { Application } from "pixi.js";
+import { GameState } from "../main";
+
+export default async function finishedContainer(
+  app: Application,
+  switchState: (state: GameState) => void
+) {
+  //
+}
