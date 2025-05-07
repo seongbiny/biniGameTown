3bd110b70ca7bc79f49dba63d0b6c77a938712f1
seongbiny <s30ngb1n@gmail.com>
2025-05-07 14:14:02 +0900

    chore: 폴더 구조 설정

--- packages/game/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { Application } from "pixi.js";
-
-(async () => {
-  const app = new Application();
-
-  await app.init({ background: "#1099bb", resizeTo: window });
-
-  document.body.appendChild(app.canvas);
-})();
