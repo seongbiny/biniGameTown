4d7af37e7692eaff4d1a7fe09aa40c19c281eb7b
seongbiny <s30ngb1n@gmail.com>
2025-05-15 17:49:44 +0900

    feat: 불필요한 콘솔 삭제

--- packages/game/bini-puzzle/src/game/utils/calculateStageSize.ts
+++ packages/game/bini-puzzle/src/game/utils/calculateStageSize.ts
@@ -16,9 +16,5 @@ export const calculateStageSize = (
   const calculatedWidth = width * scale;
   const calculatedHeight = height * scale;
 
-  console.warn(
-    `calculateStageSize : ${calculatedWidth}, ${calculatedHeight}, ${scale}`
-  );
-
   return { width: calculatedWidth, height: calculatedHeight, scale: scale };
 };
