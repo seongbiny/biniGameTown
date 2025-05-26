1ac4f9cf137bd717798404e602c7c287d9dd81c5
seongbiny <s30ngb1n@gmail.com>
2025-05-26 16:35:23 +0900

    feat: typo-trap 패키지 추가 및 초기 설정 완료

---
+++ packages/game/typo-trap/src/main.ts
@@ -0,0 +1,39 @@
+import { Application } from "pixi.js";
+import { calculateStageSize } from "./utils/calculateStageSize";
+
+export class App {
+  private app: Application = new Application();
+
+  public size: { width: number; height: number; scale: number } = {
+    width: 0,
+    height: 0,
+    scale: 1,
+  };
+
+  public async initialize() {
+    this.size = calculateStageSize(450, 800);
+
+    await this.app.init({
+      width: this.size.width,
+      height: this.size.height,
+      background: "#333333",
+    });
+    this.app.stage.scale.set(this.size.scale);
+
+    const container = document.getElementById("app")!;
+    container?.appendChild(this.app.canvas);
+
+    window.addEventListener("resize", () => {
+      this.size = calculateStageSize(450, 800);
+      this.app.renderer.resize(this.size.width, this.size.height);
+      this.app.stage.scale.set(this.size.scale);
+    });
+  }
+
+  public getApplication(): Application {
+    return this.app;
+  }
+}
+
+const app = new App();
+app.initialize();
