d847a47daf08a4de3700524390e67da70fae1ad0
seongbiny <s30ngb1n@gmail.com>
2025-05-30 11:09:45 +0900

    feat: typo-trap 패키지에 Vite 설정 파일 추가 및 빌드 구성 정의

---
+++ packages/game/typo-trap/vite.config.ts
@@ -0,0 +1,13 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [],
+  assetsInclude: ["**/*.otf", "**/*.ttf", "**/*.json"],
+  base: "/game/typo-trap/",
+  build: {
+    assetsInlineLimit: 0,
+    outDir: resolve(__dirname, "../../../dist/game/typo-trap"),
+    emptyOutDir: true,
+  },
+});
